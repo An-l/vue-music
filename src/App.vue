6f87daf734d98a36287481038f2fb9a3b0ed16ef
an-l <53218936@qq.com>
2017-10-14 16:43:11 +0800

    初始化项目目录

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+
+export default {
+  name: 'app',
+  components: {
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'common/scss/variable.scss';
+
+#app {
+  color: $color-theme;
+}
+</style>
